111e4c2a908c6f4d8a6d0f20e26af08b1b9d76a3
Uladox <uladoxiental@gmail.com>
2016-05-18 21:51:52 -1000

    adds stuff from other gits

---
+++ swm/config.h
@@ -0,0 +1,18 @@
+#define MOD		SUPER		/* modifier for mouse resize and move */
+
+/* INNER + OUTER should not be greater than BORDERWIDTH */
+#define BORDERWIDTH	4		/*  full  border width */
+#define INNER		3		/*  inner border width */
+#define OUTER		1		/*  outer border width */
+
+/* colors */
+#define FOCUSCOL	0x563C5C
+#define UNFOCUSCOL	0x000000
+#define OUTERCOL	0xFFFFFF
+
+/* resize and move by mouse? */
+#define ENABLE_MOUSE
+/* sloppy focus? */
+#define ENABLE_SLOPPY
+/* and double borders? */
+#define DOUBLE_BORDER
